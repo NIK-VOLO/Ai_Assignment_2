a86377e9e23ca122e95f5225e751937e9fab8835
Kunal Thakker <62309595+kt398@users.noreply.github.com>
2020-10-24 17:50:52 -0400

    Got text to wrok

--- map_cell.py
+++ map_cell.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
+RED=(255,0,0)
 class Ctype(Enum):
     MAGE=1
     WUMPUS=2
@@ -23,6 +23,9 @@ class Cell:
         self.y=row*size
         self.ctype=ctype
         self.selected=False
+        self.outerRect=pygame.Rect(self.x,self.y,self.size,self.size)
+        self.innerRect=pygame.Rect(self.x+(self.size/12),self.y+(self.size/12),self.size-(self.size/6),self.size-(self.size/6))
+        #self.font = pygame.font.SysFont(NONE, 12)
 
 
     #Called when a cell is clicked on, 
@@ -32,11 +35,32 @@ class Cell:
     def set_ctype(self,ctype):
         self.ctype=ctype
 
-
+    def get_type_text(self):
+        t=self.ctype
+        if(t==Ctype.CPUKNIGHT):
+            return 'CPUK'
+        elif(t==Ctype.CPUMAGE):
+            return 'CPUM'
+        elif(t==Ctype.CPUWUMPUS):
+            return 'CPUW'
+        elif(t==Ctype.MAGE):
+            return "PM"
+        elif(t==Ctype.KNIGHT):
+            return "PK"
+        elif(t==Ctype.WUMPUS):
+            return 'PW'
+        elif(t==Ctype.HOLE):
+            return 'HOLE'
+        else:
+            return ''
     def draw(self,win):
         print('draw')
+        font=pygame.font.SysFont(None,20)
+        text=font.render(f'Test{self.get_type_text()}',True,RED)
+        innerRect = text.get_rect(center=self.innerRect.center)
         #Edit this based on what the cell currently contains
-        #pygame.draw.rect(win,BLACK,(self.x,self.y,self.size,self.size))
-        pygame.draw.rect(win,WHITE,(self.x+(self.size/10),self.y+(self.size/10),self.size-(self.size/5),self.size-(self.size/5)))
+        pygame.draw.rect(win,BLACK,(self.x,self.y,self.size,self.size))
+        pygame.draw.rect(win,WHITE,(self.x+(self.size/12),self.y+(self.size/12),self.size-(self.size/6),self.size-(self.size/6)))
+        win.blit(text,innerRect)
         # pygame.draw.rect(win,,(self.x,self.y,self.size,self.size))
         
